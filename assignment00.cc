a8401420be0118c3342d6849ba60f01d83273804
Kevin Gebert <z1904317@hopper.cs.niu.edu>
2022-01-25 16:12:13 -0600

    added code to assign file and made makefile

--- assignment00.cc
+++ assignment00.cc
@@ -5,3 +5,12 @@ csci 480
 I certify that this is my own work and where appropriate an extension 
 of the starter code provided for the assignment.
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Data Structures are important!!" << endl;
+    return 0;
+}
